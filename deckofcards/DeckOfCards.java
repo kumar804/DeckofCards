b5085cdb7ef889ef02c0ab1f8ab527d05219a884
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-30 09:57:45 +0530

    creating combination

--- deckofcards/DeckOfCards.java
+++ deckofcards/DeckOfCards.java
@@ -1,7 +1,26 @@
 package deckofcards;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DeckOfCards {
+
+
     public static void main(String[] args) {
-        System.out.println("welcome to deck of cards");
+        String[][] player = new String[4][9];
+        String[][] cardCombo = new String[4][13];
+        Set<String> cards = new HashSet();
+        String[] suits = {"Diamond", "Heart", "Spades", "Club"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack" , "Queen", "King", "Ace"};
+
+
+        //Create combination of cards
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                cardCombo[i][j] = suits[i] + rank[j];
+            }
+        }
+
+
     }
 }
